4e094a4014afe6bcd7ba9ba74622d8ddc998d57d
golipathrakib <rakib.488125@gmail.com>
2020-07-19 09:02:17 +0600

    javascriptBankProject

---
+++ pioneer.js
@@ -0,0 +1,20 @@
+//login button event handler
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = ParseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    console.log(totalDeposit);
+})
