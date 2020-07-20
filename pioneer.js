9512f25d3009a10927fcd7f1574fb94744ec3edf
golipathrakib <rakib.488125@gmail.com>
2020-07-20 22:11:47 +0600

    Band Deposit and Balance

--- pioneer.js
+++ pioneer.js
@@ -10,11 +10,33 @@ loginBtn.addEventListener("click", function(){
 //deposit button event handler
 const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = ParseFloat(depositAmount);
+    const depositNumber = getInputNumber("depositAmount");
+    // const depositAmount = document.getElementById("depositAmount").value;
+    // const depositNumber = parseFloat(depositAmount);
 
-    const currentDeposit = document.getElementById("currentDeposit").innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-    console.log(totalDeposit);
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+
+    document.getElementById("depositAmount").value = "";
 })
+
+// withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    console.log(withdrawNumber);
+    
+})
+
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+   document.getElementById(id).innerText = totalAmount;
+}
